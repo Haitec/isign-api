bc521e482a5965fe7cdfe1331516eb211d1a37fc
Miguel Dönicke <miguel@doenicke.io>
2018-11-05 01:42:53 +0100

    Check and change status

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,14 +2,31 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
+    
+    struct Status: Content {
+        let id: Int
+        let isOk: Bool
     }
     
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    struct StatusRequest: Content {
+        let id: Int
+        let isOk: Bool
+    }
+    
+    var isOk = true
+    
+    router.get("status", Int.parameter) { req -> Status in
+        let id = try req.parameters.next(Int.self)
+        return Status(id: id, isOk: isOk)
+    }
+    
+    router.post("status") { req -> Future<HTTPStatus> in
+        return try req.content.decode(StatusRequest.self).map(to: HTTPStatus.self) { statusRequest in
+            print(statusRequest.id)
+            print(statusRequest.isOk)
+            isOk = statusRequest.isOk
+            return .ok
+        }
     }
 
     // Example of configuring a controller
