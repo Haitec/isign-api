b909191cac790c36cba22b9cb994da831cddaf83
Miguel Dönicke <miguel@doenicke.io>
2018-11-05 02:11:53 +0100

    CORS

--- Sources/App/app.swift
+++ Sources/App/app.swift
@@ -5,6 +5,18 @@ public func app(_ env: Environment) throws -> Application {
     var config = Config.default()
     var env = env
     var services = Services.default()
+    
+    var middlewares = MiddlewareConfig()
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
+    middlewares.use(corsMiddleware)
+    middlewares.use(ErrorMiddleware.self)
+    services.register(middlewares)
+    
     try configure(&config, &env, &services)
     let app = try Application(config: config, environment: env, services: services)
     try boot(app)
